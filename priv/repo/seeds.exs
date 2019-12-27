424e3fc1a9d75faa57425dfce7b7de1e91fb0b12
barthr <bartfokker@protonmail.com>
2019-12-27 10:52:11 +0100

    update seed.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,7 @@
 # and so on) as they will fail if something goes wrong.
 %Updtr.Accounts.User{}
 |> Updtr.Accounts.User.changeset(%{
-  email: "test1",
-  password: "test1"
+  email: "1@1.com",
+  password: "testingthisout"
 })
 |> Updtr.Repo.insert!()
