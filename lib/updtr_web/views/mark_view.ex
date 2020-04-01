2c2c37f2323d0026d41fea8f98226119f23d9819
Bart <info@bartfokker.com>
2020-04-02 00:13:26 +0200

    add icons and navigation for marks-live cards

--- lib/updtr_web/views/mark_view.ex
+++ lib/updtr_web/views/mark_view.ex
@@ -1,3 +1,11 @@
 defmodule UpdtrWeb.MarkView do
   use UpdtrWeb, :view
+
+  def random_color() do
+    [
+      "red", "orange", "yellow", "olive", "green",
+      "teal", "blue", "violet", "purple", "pink",
+      "brown", "grey", "black"
+    ] |> Enum.random()
+  end
 end
